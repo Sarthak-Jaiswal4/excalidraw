790920c2a205c7a86a8f58e44bc3eaa0056b5449
Sarthak Jaiswal <sarthakjazz8@gmail.com>
2026-04-11 23:18:39 +0530

    deployed on render-9

--- packages/DB/src/index.ts
+++ packages/DB/src/index.ts
@@ -1,3 +1,14 @@
-import { PrismaClient } from "@prisma/client"
+import "dotenv/config";
+import { Pool } from "pg";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
 
-export const prismaClient=new PrismaClient()
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+const adapter = new PrismaPg(pool as any);
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+export const prismaClient = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prismaClient;
